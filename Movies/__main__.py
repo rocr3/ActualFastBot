c73b3fd8ff5b735003ddd2e75c6f8e5ec80e5f89
Rexfilers <138610025+Rexfilers@users.noreply.github.com>
2023-09-04 14:12:02 +0530

    Create __main__.py

---
+++ Movies/__main__.py
@@ -0,0 +1,85 @@
+from os import environ
+from config import API_HASH, API_ID, BOT_TOKEN, LOG_STR, streambot
+from aiohttp import web
+from pyrogram import types
+from typing import Union, Optional, AsyncGenerator
+from pyrogram.raw.all import layer
+from pyrogram import Client, __version__
+import logging
+import logging.config
+from Movies.database.media import Media
+from Movies.utils import temp
+from Movies.database.chats import db
+from server import web_server
+
+# ==============> logging <=============== #
+
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
+
+PORT = environ.get("PORT", "8080")
+
+# ==============> movies-client <=============== #
+
+
+class Movies(Client):
+
+    def __init__(self):
+        super().__init__(
+            ":Proton-Movies:",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "Movies.plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
+        await streambot.start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(
+            f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
+
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
+
+
+app = Movies()
+app.run()
+          
