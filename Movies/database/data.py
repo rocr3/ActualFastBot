fcedf76d62f1e3c041fecb15dfefd0761e3414f0
Rexfilers <138610025+Rexfilers@users.noreply.github.com>
2023-09-04 14:36:23 +0530

    Create data.py

---
+++ Movies/database/data.py
@@ -0,0 +1,34 @@
+import motor.motor_asyncio
+from config import AUTH_CHANNEL, USER_DATABASE_NAME, USER_DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, SHORTLINK_API, SHORTLINK_URL
+
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        self.grp = self.db.groups
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def total_chat_count(self):
+        count = await self.grp.count_documents({})
+        return count
+
+    async def get_all_chats(self):
+        return self.grp.find({})
+
+    async def get_db_size(self):
+        return (await self.db.command("dbstats"))['dataSize']
+
+
+db = Database(USER_DATABASE_URI, USER_DATABASE_NAME)
